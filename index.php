4bf07d4bb5e54a2e1a6f272f8eaae0533a9275a9
microsoftjulius <julisema4@gmail.com>
2020-02-11 07:09:28 -0500

    Added FILE_APPEND flag, 10th/02/2020

--- index.php
+++ index.php
@@ -1,7 +1,8 @@
 <?php
 function saveContent(){
-	$url = "http://church.pahappa.com/login";
-	file_put_contents('data.txt', file_get_contents($url));
+	$url = "php://input"; //reads from any url
+	file_put_contents('data.txt', file_get_contents($url), FILE_APPEND); //file append is optional. dont include it incase you want to completly 
+	//overide the existing data
 }
 saveContent();
 function readContent(){
